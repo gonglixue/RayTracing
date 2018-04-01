4fc152b24ad8166b9b8d8ef9e523bd0904b5d064
gonglixue <724351282@qq.com>
2018-04-01 20:25:08 +0800

    emissive material

---
+++ RayTracing/Emissive.cpp
@@ -0,0 +1,65 @@
+#include "Emissive.h"
+
+Emissive::Emissive():
+	Material(),
+	ls(1.0),
+	color(1.0)
+{}
+
+Emissive::Emissive(const Emissive& e):
+	Material(e),
+	ls(e.ls),
+	color(e.color)
+{}
+
+Emissive& Emissive::operator= (const Emissive& rhs)
+{
+	if (this == &rhs)
+		return (*this);
+
+	Material::operator=(rhs);
+	ls = rhs.ls;
+	color = rhs.color;
+	return (*this);
+}
+
+Emissive::~Emissive() {
+
+}
+
+// 当入射方向与对象表面法线位于同一侧时，返回颜色
+glm::vec3 Emissive::area_light_shade(ShadeRec& sr)
+{
+	if (glm::dot(-sr.normal, glm::vec3(sr.ray.d)) > 0.0)
+		return ls * color;
+	else
+		return BLACK;
+}
+
+glm::vec3 Emissive::shade(ShadeRec& sr)
+{
+	if (glm::dot(-sr.normal, glm::vec3(sr.ray.d)) > 0.0)
+		return ls * color;
+	else
+		return BLACK;
+}
+
+glm::vec3 Emissive::get_Le(ShadeRec& sr) const
+{
+	return ls * color;
+}
+
+glm::vec3 Emissive::path_shade(ShadeRec& sr)
+{
+	return ls * color;
+}
+
+glm::vec3 Emissive::global_shade(ShadeRec& sr)
+{
+	if (sr.depth == 1)
+		return BLACK;
+	if (glm::dot(-sr.normal, glm::vec3(sr.ray.d)) > 0.0)
+		return ls * color;
+	else
+		return BLACK;
+}
--- RayTracing/Emissive.h
+++ RayTracing/Emissive.h
@@ -1,4 +1,4 @@
-#if 0
+#if 1
 #ifndef __EMISSIVE_H__
 #define __EMISSIVE_H__
 #include "Material.h"
@@ -22,9 +22,25 @@ public:
 
 	virtual glm::vec3 get_Le(ShadeRec& sr) const;
 	virtual glm::vec3 shade(ShadeRec& sr);
+	virtual glm::vec3 area_light_shade(ShadeRec& sr);
+	virtual glm::vec3 path_shade(ShadeRec& sr);
+	virtual glm::vec3 global_shade(ShadeRec& sr);
 
 
 };
+
+inline void Emissive::set_radiance(const float _ls) {
+	ls = _ls;
+}
+
+inline void Emissive::set_color(float r, float g, float b) {
+	color = glm::vec3(r, g, b);
+}
+
+inline void Emissive::set_color(const glm::vec3& c)
+{
+	color = c;
+}
 
 #endif // !__EMISSIVE_H__
 
